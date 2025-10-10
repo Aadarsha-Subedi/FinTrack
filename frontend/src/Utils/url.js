7d9aa37dfe1e56b05f520df295e3763337330a2b
Aadarsha Subedi <aadarshasubedi20@gmail.com>
2025-10-10 18:46:32 +0545

    url swap

--- frontend/src/Utils/url.js
+++ frontend/src/Utils/url.js
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:8000';
-// export const url = 'https://finance-tracker-backend-zjf4.onrender.com';
+// export const url = 'http://localhost:8000';
+export const url = 'https://finance-tracker-backend-zjf4.onrender.com';
